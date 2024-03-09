0ec389d5b40e1e9949b11c7244859b9b4b5037f6
VSCPlays <142426954+gamma-sigma-beep@users.noreply.github.com>
2024-03-09 23:03:31 +0400

    Create isAdmin.lua

---
+++ src/main/isAdmin.lua
@@ -0,0 +1,4 @@
+return function(player: Player)
+	-- This must be changed as it checks if the player in my group's rank is higher than one, you should change it to your group or whatever
+	return player --player:GetRankInGroup(13934532) > 1
+end
